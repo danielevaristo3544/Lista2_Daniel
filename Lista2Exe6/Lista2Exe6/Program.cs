e0924cd76333f260bb4bd2112ea24bf9b289f0c5
danielevaristo3544 <daniel.evaristo@aluno.ifsp.edu.br>
2026-04-29 15:19:49 -0300

    Add files via upload

---
+++ Lista2Exe6/Lista2Exe6/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista2Exe6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Programa dedicado a calcular a relação peso e altura de uma pessoa e exibir uma mensagem da situação
+
+            int p;
+            int a;
+            int r;
+
+            Console.WriteLine(" Programa para calcular a relação peso e altura de uma pessoa ");
+            Console.Write(" Digite o seu peso em Kg: ");
+            p = int.Parse(Console.ReadLine());
+            Console.Write(" Digite a sua altura em cm: ");
+            a = int.Parse(Console.ReadLine());
+
+            double a1 = a / 100.0;  // Convertendo a altura de cm para metros    
+            r = (int)(p / (a1 * a1));
+
+            if (r < 20)
+            {
+                Console.WriteLine(" Abaixo do peso ");
+            }
+            else
+            {
+                if (20 <= r && r < 25)
+                {
+                    Console.WriteLine(" O peso é ideal ");
+                }
+                else
+                {
+                    Console.WriteLine(" Acima do peso ");
+                }
+            }
+        }
+    }
+}
+
---
+++ Lista2Exe8/Lista2Exe8/Program.cs
@@ -0,0 +1,39 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista2Exe8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Programa dedicado a identificar um triângulo retângulo através de 03 entradas
+
+            int a;
+            int b;
+            int c;
+
+
+            Console.WriteLine(" Programa para identificar um triângulo retângulo através de suas 03 entradas ");
+            Console.Write(" Digite o 1º lado: ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write(" Digite o 2º lado: ");
+            b = int.Parse(Console.ReadLine());
+            Console.Write(" Digite o 3º lado: ");
+            c = int.Parse(Console.ReadLine());
+
+            if ((a * a) + (b * b) == (c * c) || (a * a) + (c * c) == (b * b) || (b * b) + (c * c) == (a * a))
+            {
+                Console.WriteLine(" Os lados formam um triângulo retângulo ");
+            }
+            else
+            {
+                Console.WriteLine(" Os lados não formam um triângulo retângulo ");
+            }
+        }
+    }
+}
