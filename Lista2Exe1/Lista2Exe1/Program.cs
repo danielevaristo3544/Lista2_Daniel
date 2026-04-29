a54cbfe521949881dc370552a3c7d4f908991b79
danielevaristo3544 <daniel.evaristo@aluno.ifsp.edu.br>
2026-04-29 15:10:38 -0300

    Add files via upload

---
+++ Lista2Exe1/Lista2Exe1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista2Exe1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+
+            Console.Write("Digite o 1º número:");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Digite o 2º número:");
+            b = int.Parse(Console.ReadLine());
+
+            if (a > b)
+            {
+                Console.WriteLine("O 1º número é maior:");
+            }
+            else
+            {
+                Console.WriteLine("O 2º múmero é maior:");
+            }
+
+        }
+    }
+}
---
+++ Lista2Exe2/Lista2Exe2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista2Exe2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;       //  Programa para ler dois números inteiros e dizer qual é o maior ou se são iguais
+            int b;
+
+            Console.Write("Digite o 1º número inteiro:");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Digite o 2º número inteiro:");
+            b = int.Parse(Console.ReadLine());
+
+            if (a == b)
+            {
+                Console.WriteLine("Os números são iguais");
+            }
+            else
+            {
+                if (a > b)
+                {
+                    Console.WriteLine("O 1º número é maior.");
+                }
+                else
+                {
+                    Console.WriteLine("O 2º número é maior.");
+                }
+            }
+        }
+    }
+}
